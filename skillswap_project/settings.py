f56d4b62b3ee0a559a4f0130500f3fe7bfa0efa0
Evence Mohau Langa <evencel@netninenine.co.za>
2025-10-25 21:57:10 +0200

    Prepare for Render deployment

--- skillswap_project/settings.py
+++ skillswap_project/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'replace-this-with-a-secure-secret-key'
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'channels',
